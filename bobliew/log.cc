0f8f433f757ea4be5921a02547581d66b6023d8a
Bobliew <minghaoliao@qq.com>
2022-08-09 09:14:44 +0800

    v2

--- bobliew/log.cc
+++ bobliew/log.cc
@@ -1,84 +1,18 @@
-#ifndef __BOBLIEW_LOG__
-#define __BOBLIEW_LOG__
-
-
-#include <cctype>
-#include <string>
-#include <memory>
-#include <stdint.h>
-
+#include "log.h"
 
 namespace bobliew{
 
-class LogEvent {
-public:
-    typedef std::shared_ptr<LogEvent> ptr;
-    LogEvent();
-private:
-    const char* m_file = nullptr;
-    int32_t m_line = 0;
-    uint32_t m_elapse = 0;
-    uint32_t m_threadId = 0;
-    uint32_t m_fiberId = 0;
-    uint64_t m_time = 0;
-    std::string m_content;
-};
+Logger::Logger(const std::string& name="root");
+void log(LogLevel::Level level, const LogEvent::ptr event);
+
+void debug(LogEvent::ptr event);
+void info(LogEvent::ptr event);
+void warn(LogEvent::ptr event);
+void error(LogEvent::ptr event);
+void fatal(LogEvent::ptr event);
 
 
-class LogLevel{
-public:
-    enum Level{
-        DEBUG = 1,
-        INFO = 2,
-        WARN = 3,
-        ERROR = 4,
-        FATAL = 5,
-    };
-};
-
-class LogFormatter {
-public:
-    typedef std::shared_ptr<LogFormatter> ptr;
-
-    std::string format(LogEvent::ptr event);
-private:
-}; 
-
-
-//日志输出地
-class LogAppender {
-public:
-    typedef std::shared_ptr<LogAppender> ptr;
-    virtual ~LogAppender(){}
-
-    void log(LogLevel::Level level, LogEvent::ptr event);
-private:
-    LogLevel::Level m_level;
-};
-
-//日志器
-class Logger {
-public:
-    typedef std::shared_ptr<Logger> ptr;
-    Logger(const std::string& name="root");
-
-    void log(LogLevel::Level level, const LogEvent::ptr event);
-
-private:
-    std::string m_name;
-    LogLevel::Level m_level;
-    LogAppender::ptr m_ptr;
-    
-};
-
-class StdoutLogAppender : public LogAppender {
-};
-
-class FileLogAppender : public LogAppender {
-};
-
-
+void addAppender(LogAppender::ptr appender);
+void delAppender(LogAppender::ptr appender);
 
 }
-
-#endif
